426691e040a7829b74a728b67eb8521d68b7d085
Aleksandr Semikin <aesemikin@alice-a100.sas.yp-c.yandex.net>
2025-09-30 20:22:25 +0300

    done tasks

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Any, Iterable, List, Tuple
 
+from copy import copy
+
 from typing_extensions import Protocol
 
 # ## Task 1.1
@@ -22,8 +24,11 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    vals_plus = copy(vals)
+    vals_plus[arg] += epsilon
+    vals_minus = copy(vals)
+    vals_minus[arg] -= epsilon
+    return (f(vals_plus) - f(vals_minus)) / (2 * epsilon)
 
 
 variable_count = 1
@@ -51,6 +56,17 @@ class Variable(Protocol):
         pass
 
 
+def search(v: Variable, res: list, vis: set) -> None:
+    if v.unique_id in vis:
+        return None
+    vis.add(v.unique_id)
+    if not v.is_constant():
+        for par in v.parents:
+            search(par, res, vis)
+    res.append(v)
+    return None
+
+
 def topological_sort(variable: Variable) -> Iterable[Variable]:
     """
     Computes the topological order of the computation graph.
@@ -61,8 +77,10 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     Returns:
         Non-constant Variables in topological order starting from the right.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError("Need to implement for Task 1.4")
+    vis = set()
+    res: list[Variable] = []
+    search(variable, res, vis)
+    return res
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -76,8 +94,19 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError("Need to implement for Task 1.4")
+    grad = {}
+    vars = reversed(topological_sort(variable))
+    grad[variable.unique_id] = deriv
+    for v in vars:
+        v_grad = grad[v.unique_id]
+        if v.is_leaf():
+            v.accumulate_derivative(v_grad)
+        else:
+            if not v.is_constant():
+                for input_var, input_grad in v.chain_rule(v_grad):
+                    if input_var.unique_id not in grad:
+                        grad[input_var.unique_id] = 0
+                    grad[input_var.unique_id] += input_grad
 
 
 @dataclass
